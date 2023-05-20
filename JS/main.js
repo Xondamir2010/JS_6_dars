56a76324bbb5379e42694eaf2c347de11311d392
Xondamir2010 <shohruhquvvarov@gmail.com>
2023-05-20 08:07:51 +0500

    JS 6 dars kodlari
    JS 6 dars kodlari yuklandi

---
+++ JS/main.js
@@ -0,0 +1,51 @@
+/*let headTag = document.head;
+let bodyTag = document.body;
+let havola = document.documentURI;
+let formalar = document.forms*/
+/*console.log(formalar);
+* */
+/*function battonniBos(){
+    alert("Siz battonni bir marta bosdingiz!")
+}
+
+function battonniIkkiMartaBos(){
+    alert("Siz battonni ikki marta bosdingiz!")
+}*/
+/*function tanlash(event){
+    alert("Siz " + event.target.value + "ni tanladingiz!")
+}
+
+function inputgaBosish(){
+    console.log("Inputga bosildi!")
+}
+
+function inputdanChiqish(){
+    console.log("Inputdan chiqildi!")
+}
+
+function yozish(event){
+    console.log(event.target.value)
+}
+
+document.querySelector(".classNomi").addEventListener('copy',function (event){
+    console.log("Diqqat! Sizdan " + event.target.value + "nomli fayllar ko'chirilmoqda!");
+})*/
+
+function harakatlanish(e){
+    console.log(e.clientX,e.clientY);
+}
+
+function kirish(){
+    console.log("Hududga kirildi!");
+}
+
+function chiqish(){
+    console.log("Hududdan chiqildi!")
+}
+
+
+
+
+
+
+
